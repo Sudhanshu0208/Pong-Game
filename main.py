bd62289af7e1f1b1e52ec81981c3a919d34cfc8d
Sudhanshu Goswami <sud0208@gmail.com>
2021-09-25 18:54:44 +0530

    Basic Users Mode

---
+++ main.py
@@ -0,0 +1,48 @@
+import turtle
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_on = True
+while game_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_wall()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() == 330 or ball.distance(l_paddle) < 50 and ball.xcor() == -330:
+        ball.bounce_paddle()
+
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.update()
+        scoreboard.r_point()
+
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.update()
+        scoreboard.l_point()
+
+
+screen.exitonclick()
